6bc808c89d767bad464917fd65b072ec7b5ed616
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2022-06-25 18:23:32 +0800

    update npm progress-bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import '../styles/bootstrap.css'
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <NextNProgress/>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
