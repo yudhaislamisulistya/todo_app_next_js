5558e270dc16fbaf49d56157bfbdcf5b1326fc5b
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2022-06-26 01:59:01 +0800

    asdasd

--- components/card.js
+++ components/card.js
@@ -3,6 +3,7 @@ const Card = ({todo}) => {
     return(
         <div className="card text-left mt-2">
             <div className="card-body">
+                halo
             <h4 className="card-title">{heading}</h4>
             <p className="card-text">{description}</p>
             </div>
